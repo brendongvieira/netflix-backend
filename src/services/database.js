19747bd9a466fee3db17a7b7c658fbdc0835fd99
Brendon Vieira <brendon-vieira@live.com>
2022-05-11 23:44:16 -0300

    first commit

--- src/services/database.js
+++ src/services/database.js
@@ -1,6 +1,9 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/netflix", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(
+  "mongodb+srv://brendongvieira:Umdiadechuva7@cluster0.aaagw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
